a965c3009129d30c0676f1bf023c1e730042acef
Mayur Sonkusare <mayursonkusare1155@gmail.com>
2023-05-06 14:16:01 +0530

    [Mayur added Program Cs file]

---
+++ DayEightProblemsWithOopsConcept/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DayEightProblemsWithOopsConcept
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("WellCome To Employee Wage Computation ");
+        }
+    }
+}
